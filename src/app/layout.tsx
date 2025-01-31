126e0cee9e4729f1f8ec68b5f0f1612b685ac95b
Vishnu Muthyalu <vm17college@gmail.com>
2025-01-30 20:30:14 -0600

    added leaders , changed logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Global Citizens Association - UTSA",
+  title: "Global Citizens' Association",
   description: "Fostering meaningful connections and cultural exchange among UTSA students",
 };
 
